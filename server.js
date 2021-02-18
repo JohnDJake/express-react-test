21c719f975839aee6b3f02a3eb6878fc31d7e76a
JohnDJake <john.d.jake@gmail.com>
2021-02-18 15:07:24 -0800

    mock api route

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const PORT = process.env.PORT || 3001;
 
 if (process.env.NODE_ENV === "production") app.use(express.static("client/build"));
 
+app.get("/api/data", (req, res) => res.json(["data1", "data2"]));
+
 app.get((req, res) => res.sendFile(path.join(__dirname, "./client/build/index.html")));
 
 app.listen(PORT, () => console.log(`API Server now listening on PORT ${PORT}`));
