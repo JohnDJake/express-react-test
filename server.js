4586eca4b60a6308831dd54d0a05711090397be6
JohnDJake <john.d.jake@gmail.com>
2021-02-18 20:07:09 -0800

    mock api sends objects instead of strings

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3001;
 
 if (process.env.NODE_ENV === "production") app.use(express.static("client/build"));
 
-app.get("/api/data", (req, res) => res.json(["data1", "data2"]));
+app.get("/api/data", (req, res) => res.json([{ id: 1, title: "data1" }, { id: 2, title: "data2" }]));
 
 app.get((req, res) => res.sendFile(path.join(__dirname, "./client/build/index.html")));
 
