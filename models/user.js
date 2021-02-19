c1c988f8e77969942c5aaabd8091baaad035fc96
JohnDJake <john.d.jake@gmail.com>
2021-02-18 22:31:59 -0800

    created user model

---
+++ models/user.js
@@ -0,0 +1,16 @@
+module.exports = (sequelize, DataTypes) => sequelize.define("User", {
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+});
