9e7fa5a98c40e12cf1e14bf1bc25c7f5eda07bd7
JohnDJake <john.d.jake@gmail.com>
2021-02-18 20:07:31 -0800

    display mock api response instead of console logging it

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,34 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 
 function App() {
-    useEffect(() => axios.get("/api/data").then(({ data }) => console.log(data)), []);
+    const [apiState, setApiState] = useState([]);
+
+    useEffect(() => axios.get("/api/data").then(({ data }) => setApiState(data)), []);
 
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>Edit <code>src/App.js</code> and save to reload.</p>
+                <p>API Data:</p>
+                <ol>
+                    {apiState.map(obj => <li key={obj.id}>{obj.title}</li>)}
+                </ol>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
